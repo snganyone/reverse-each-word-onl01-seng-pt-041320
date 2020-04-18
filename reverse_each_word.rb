b96eb78c8f0bc6444cc591948e649a10683bb191
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-18 03:47:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,7 @@ def reverse_each_word(sentence)
   arr.each do |i|
     #print i.reverse!
     new_arr << i
-    new_arr.join(" ")
   end
-  #new_arr.join(" ")
+  new_arr.join(" ")
   #print new_arr.reverse!
 end
