4287d5447c4d04a480deb635851aaf8f013bf37a
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-18 04:04:31 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  arr = sentence.split(" ")
+  new_arr = []
+  arr.collect do |word|
+    #print i.reverse!
+    len = word.length
+    words = ""
+    while len > 0
+      len -= 1
+      words += word[len] #Reverses every word in array
+    end
+    new_arr << words #Add words to the new array
+  end
+  new_arr.join(" ") #Joins array
+end
